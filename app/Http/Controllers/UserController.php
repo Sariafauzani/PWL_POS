56e876e875cc640900f1bd9338827baa3e291283
Sariafauzani <ajas2654@gmail.com>
2025-03-13 22:21:22 +0700

    Prak 6_Eloquent ORM_JS3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function showProfile($id, $name) {
-        return view('user', compact('id', 'name'));
+    public function index () {
+        // coba akses model userModel
+       // $user = UserModel::all(); // ambil semua data dari tabel m_user
+       // return view('user', ['data' => $user]);
+
+       /*$data = [
+        'username' => 'customer-1',
+        'nama' => 'Pelanggan',
+        'password' => Hash::make('12345'),
+        'level_id' => 4
+       ];
+       UserModel::insert($data); // tambahkan data ke tabel m_user
+
+       // coba akses model UserModel
+       $user = UserModel::all(); // ambil semua data dari tabel m_user
+       return view('user', ['data' => $user]);*/
+
+       $data = [
+        'nama' => 'Pelanggan Pertama',
+       ];
+       UserModel::where('username', 'customer-1')->update($data); // update data user
+
+       // coba akses model UserModel
+       $user = UserModel::all(); // ambil semua data dari tabel m_user
+       return view('user', ['data' => $user]);
     }
 }
