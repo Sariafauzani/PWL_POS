f6fc40a31cd24223eda33ebcc39e243892b68232
Sariafauzani <ajas2654@gmail.com>
2025-03-14 22:45:22 +0700

    Prak 2.5 - Langkah 1_JS4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index () {
 
-       $user = UserModel::firstOrNew(
+       $user = UserModel::create(
         [
             'username' => 'manager55',
             'nama' => 'Manager55',
