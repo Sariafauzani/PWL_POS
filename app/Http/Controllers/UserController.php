9f0e5239204a6c12b04493a823458f3e49616557
Sariafauzani <ajas2654@gmail.com>
2025-03-14 22:53:25 +0700

    Prak 2.5 - Attribute Changes _ JS4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,23 +12,19 @@ class UserController extends Controller
 
        $user = UserModel::create(
         [
-            'username' => 'manager55',
-            'nama' => 'Manager55',
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
-            'level_id' => 2
-        ],
-       );
-       $user->username = 'manager56';
-
-       $user->isDirty(); // true
-       $user->isDirty('username'); // true
-       $user->isDirty('name'); // false
-       $user->isDirty(['nama', 'username']); // false
+            'level_id' => 2,
+        ]);
+       $user->username = 'manager12';
         
        $user->save();
 
-       $user->isDirty(); // false
-       $user->isDirty(); // true
-       dd($user->isDirty());
+       $user->wasChanged(); // true
+       $user->wasChanged('username'); // true
+       $user->wasChanged(['username', 'level_id']); // true
+       $user->wasChanged('nama'); // false
+       dd($user->wasChanged(['nama', 'username'])); // true
     }
 }
