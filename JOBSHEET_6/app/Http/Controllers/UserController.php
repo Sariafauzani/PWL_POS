78c2f3f0583d20701023cfc0ef253a3e40e29b13
Sariafauzani <ajas2654@gmail.com>
2025-03-24 05:45:23 +0700

    Praktikum 1 - Modal Ajax Tambah Data User

--- JOBSHEET_6/app/Http/Controllers/UserController.php
+++ JOBSHEET_6/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\UserModel;
 use App\Models\LevelModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
@@ -186,4 +187,45 @@ class UserController extends Controller
         }
     }
 
+    public function create_ajax() {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.create_ajax')
+                    ->with('level', $level);
+    }
+
+    public function store_ajax(Request $request) {
+        // Cek apakah request berupa ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id'  => 'required|integer',
+                'username'  => 'required|string|min:3|unique:m_user,username',
+                'nama'      => 'required|string|max:100',
+                'password'  => 'required|min:6'
+            ];
+    
+            // Menggunakan Validator untuk validasi input
+            $validator = Validator::make($request->all(), $rules);
+    
+            if ($validator->fails()) {
+                return response()->json([
+                    'status'   => false, // Response status: false = error/gagal, true = berhasil
+                    'message'  => 'Validasi Gagal',
+                    'msgField' => $validator->errors() // Pesan error validasi
+                ]);
+            }
+    
+            // Simpan data ke database
+            UserModel::create($request->all());
+    
+            return response()->json([
+                'status'  => true,
+                'message' => 'Data user berhasil disimpan'
+            ]);
+        }
+    
+        // Redirect jika bukan request ajax
+        return redirect('/');
+    }
+    
 }
